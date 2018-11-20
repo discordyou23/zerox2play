c2982d3bd85fb0c67985417944a29c1bfc4a3cb5
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-11-20 20:54:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = "R";
+ const adminprefix = "R";
 client.on('ready', () => {
     console.log('I am ready!');
 });
